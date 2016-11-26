9d9c29a24148c93a7c8f6e98f6eca2be1d19ac09
Jim A <downsampling@gmail.com>
2016-11-26 22:27:11 +0000

    pick clips randomly

--- control.py
+++ control.py
@@ -1,18 +1,19 @@
 import curses
 import os
+import random
 import subprocess
 
 CMD_PLAY_ONCE = 'omxplayer -b'
 
-CLIP_DIR = '/home/pi'
-CLIP_DEFAULT = ''
-CLIP_A = 'test_640x480.mp4'
+CLIP_DIR = '/home/pi/dollhouse-clips'
+CLIP_DEFAULT = 'Default.mp4'
+CLIPS_NO = [ os.path.join(CLIP_DIR, 'No', name) for name in os.listdir(os.path.join(CLIP_DIR, 'No')) if not name.startswith('.') ]
+CLIPS_YES = [ os.path.join(CLIP_DIR, 'Yes', name) for name in os.listdir(os.path.join(CLIP_DIR, 'Yes')) if not name.startswith('.') ]
 
 
 def play_clip(clip):
 
-    clip_path = os.path.join(CLIP_DIR, clip)
-    subprocess.call('{} {}'.format(CMD_PLAY_ONCE, clip_path), shell=True)
+    subprocess.call('{} {}'.format(CMD_PLAY_ONCE, clip), shell=True)
 
 
 def main(stdscr):
@@ -20,7 +21,7 @@ def main(stdscr):
     while 1:
         c = stdscr.getch()
         if c == ord(' '):
-            play_clip(CLIP_A)
+            play_clip(random.choice(CLIPS_NO + CLIPS_NO + CLIPS_YES))
             curses.flushinp()            
 
 
